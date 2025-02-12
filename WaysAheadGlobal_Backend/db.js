b728d7fd5e2dfb984f925c8aedafb770527fb4ed
Suman <saransuman1757@gmail.com>
2025-02-12 22:39:57 +0530

    commit

--- WaysAheadGlobal_Backend/db.js
+++ WaysAheadGlobal_Backend/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {        
         await mongoose.connect(process.env.MONGO_URI);
-        // await mongoose.connect('mongodb+srv://saransuman1757:db12345@cluster0.whboc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         
         console.log("MongoDB connected"); 
     } catch (error) {
